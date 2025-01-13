1aa8d4ce171419d1f3e990b0ca09ed43b0205466
HEVERTON NASCIMENTO DE SOUZA <heverton.nsouza@senacsp.edu.br>
2025-01-13 20:04:46 -0300

    first commit

---
+++ ex.02/Program.cs
@@ -0,0 +1,7 @@
+﻿//Ex.02 - escreva um programa que pergunte o nome do usuario, armazene esse nome em uma variavel name e apresente uma mensagem de boas-vindas exibindo o nome do usuário 
+
+Console.WriteLine("informe seu nome");
+string name = Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a) " + name); 
+
---
+++ ex.03/Program.cs
@@ -0,0 +1,16 @@
+﻿// Ex-03 - escrever programa que solicite dois numeros inteiros para o usário e xiba a soma dos numeros
+
+
+Console.WriteLine("Digite um o primeiro numero");
+int numero1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um o segundo numero");
+int numero2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um o terceiro numero");
+int numero3 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Digite um o quarto numero");
+int numero4 = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"A soma é:  {numero1 + numero2 + numero3 + numero4} ");
